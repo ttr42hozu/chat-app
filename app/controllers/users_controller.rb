d41aaa491dfb276116da53a49b8dd31bde7733f2
ttr42hozu <shigi10nato@gmail.com>
2020-12-15 17:58:06 +0900

    メッセージ送信機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,10 @@ def update
   else
     render :edit
   end
-end
 
-private
+  private
 
-def user_params
-  params.require(:user).permit(:name, :email)
-end
-
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
